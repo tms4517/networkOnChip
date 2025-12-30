eb206e2f5688a16ea528481965c87abfc444bec7
tms4517 <tejminarsanghavi@hotmail.co.uk>
2025-12-30 11:39:49 +0000

    Include missing number in packet defintiion comment

--- rtl/pa_noc.sv
+++ rtl/pa_noc.sv
@@ -9,7 +9,7 @@ package pa_noc;
 
   // APB Packet Definition
   // -------------------------------------------------------------------
-  // |72                              |3              2|1              0|
+  // |72                             4|3              2|1              0|
   // |       Payload (69 bits)        |Dst Row (2 bits)|Dst Col (2 bits)|
   // |-------------------------------------------------------------------
   localparam int unsigned APB_PACKET_WIDTH = 73;
