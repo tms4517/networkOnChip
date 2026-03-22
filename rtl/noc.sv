172990deaacc18fa3206147c47707bd750e1ab67
Tej Sanghavi <Tej.Sanghavi@amd.com>
2026-03-22 07:02:16 -0400

    Add valid and ready signals to noc and mesh modules

--- rtl/noc.sv
+++ rtl/noc.sv
@@ -10,16 +10,17 @@ module noc
 , input  var logic i_arst_n
 
 , input  var logic [GRID_WIDTH-1:0][GRID_WIDTH-1:0][PACKET_WIDTH-1:0] i_niToRouter
+, input  var logic [GRID_WIDTH-1:0][GRID_WIDTH-1:0]                   i_niToRouterValid
+, output var logic [GRID_WIDTH-1:0][GRID_WIDTH-1:0]                   o_niToRouterReady
+
 , output var logic [GRID_WIDTH-1:0][GRID_WIDTH-1:0][PACKET_WIDTH-1:0] o_routerToNi
+, output var logic [GRID_WIDTH-1:0][GRID_WIDTH-1:0]                   o_routerToNiValid
+, input  var logic [GRID_WIDTH-1:0][GRID_WIDTH-1:0]                   i_routerToNiReady
 );
 
   if (GRID_WIDTH < 2) begin: ParamCheck
     $error("Grid width parameter 'GRID_WIDTH' is invalid. Must be at least 2.");
   end: ParamCheck
-
-  // NOTE: The mesh has been instanced without being connected to any network
-  // interfaces. For now, this is to test whether the mesh itself and the
-  // routing algorithms have been implemented correctly.
 
   mesh
   #(.GRID_WIDTH(GRID_WIDTH))
@@ -27,8 +28,13 @@ module noc
   ( .i_clk    (i_clk)
   , .i_arst_n (i_arst_n)
 
-  , .i_niToRouter (i_niToRouter)
-  , .o_routerToNi (o_routerToNi)
+  , .i_niToRouter      (i_niToRouter)
+  , .i_niToRouterValid (i_niToRouterValid)
+  , .o_niToRouterReady (o_niToRouterReady)
+
+  , .o_routerToNi      (o_routerToNi)
+  , .o_routerToNiValid (o_routerToNiValid)
+  , .i_routerToNiReady (i_routerToNiReady)
   );
 
 endmodule
